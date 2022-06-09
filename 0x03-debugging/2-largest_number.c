ca85068b37140b7b9edfc188c51120d39e710a60
lemidiriba <lemidiriba52@gmail.com>
2022-06-09 00:32:32 -0700

    0x03

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,22 +12,25 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && a > c)
-{
-largest = a;
-}
-else if (b > c && b > a)
-{
-largest = b;
-}
-else if (c > b)
-{
-largest = c;
-}
-else
-{
-largest = b;
-}
-
+ 
+    if (a > b)
+    {
+        if (a > c)
+        {
+            largest = a;
+        }
+        else
+        {
+          largest =c;
+        }
+    }
+    else if (b > c)
+    {
+       largest=b;
+    }
+    else
+    {
+        largest = c;
+    }
 return (largest);
 }
