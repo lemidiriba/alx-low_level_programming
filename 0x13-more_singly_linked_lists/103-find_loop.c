1bbc522009850f37179bafc3a3e858f098daa774
lemidiriba <lemidiriba52@gmail.com>
2022-07-18 23:06:36 -0700

    goiong food

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,27 +1,42 @@
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list.
+ * find_listint_loop - finds the loop in a linked list.
  * @head: head of a list.
  *
- * Return: pointer to the first node.
+ * Return: the address of the node where the loop starts.
  */
-listint_t *reverse_listint(listint_t **head)
+listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *p;
-	listint_t *n;
+	listint_t *p2;
+	listint_t *prev;
 
-	p = NULL;
-	n = NULL;
-
-	while (*head != NULL)
+	p2 = head;
+	prev = head;
+	while (head && p2 && p2->next)
 	{
-		n = (*head)->next;
-		(*head)->next = p;
-		p = *head;
-		*head = n;
+		head = head->next;
+		p2 = p2->next->next;
+
+		if (head == p2)
+		{
+			head = prev;
+			prev =  p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != head && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == head)
+					break;
+
+				head = head->next;
+			}
+			return (p2->next);
+		}
 	}
 
-	*head = p;
-	return (*head);
+	return (NULL);
 }
